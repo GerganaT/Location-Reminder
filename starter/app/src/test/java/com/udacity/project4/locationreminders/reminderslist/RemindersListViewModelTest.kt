89c003a5fc2dfc144c4c4faf2043ba5146ee6ab0
Gergana Toshkova <geritoshkova85@gmail.com>
2021-12-20 23:30:19 +0200

    start ViewModel testing

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,12 +1,23 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+
+    private lateinit var remindersListViewModel: RemindersListViewModel
+    private lateinit var fakeDataSource: FakeDataSource
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+   //TODO:Find out how to inject fakes
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
