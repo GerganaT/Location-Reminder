168ceadcd32311b131ae900ad7a9b60da4cadf9b
Gergana Toshkova <geritoshkova85@gmail.com>
2021-12-26 15:13:48 +0200

    implent ReminderListFragmentTest start

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,29 +1,68 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.FakeRemindersRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.inject
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest : AutoCloseKoinTest() {
+
+     private lateinit var fakeRemindersRepository: FakeRemindersRepository
+
+
+    @Before
+    fun initFakeRemindersRepository() {
+        stopKoin()
+        val testModule = module {
+            single { FakeRemindersRepository() }
+
+        }
+        startKoin {
+            modules(listOf(testModule))
+        }
+        fakeRemindersRepository = inject<FakeRemindersRepository>().value
+    }
+
+    @Test
+    fun clickAddReminderFab_navigateToSaveReminderFragment() {
+        //GIVEN - On the home screen
+        val fragmentScenario =
+            launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = Mockito.mock(NavController::class.java)
+        fragmentScenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+        //WHEN - Click on the add reminder fab
+        Espresso.onView(withId(R.id.addReminderFAB)).perform(ViewActions.click())
+
+        //THEN - Verify that we navigate to the save reminder screen
+        Mockito.verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
 
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
