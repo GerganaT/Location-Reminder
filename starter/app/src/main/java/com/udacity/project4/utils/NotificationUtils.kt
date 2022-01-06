e542f7a70a005b01f8053c5cd64491fe74f04645
Gergana Toshkova <geritoshkova85@gmail.com>
2022-01-06 17:24:48 +0200

    optimize code with reified functions usage

--- starter/app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
+++ starter/app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
@@ -21,6 +21,7 @@ import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
+import androidx.core.content.getSystemService
 import com.udacity.project4.BuildConfig
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.ReminderDescriptionActivity
@@ -31,11 +32,11 @@ private const val NOTIFICATION_CHANNEL_ID = BuildConfig.APPLICATION_ID + ".chann
 
 fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
     val notificationManager = context
-        .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        .getSystemService<NotificationManager>()
 
     // We need to create a NotificationChannel associated with our CHANNEL_ID before sending a notification.
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-        && notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null
+        && notificationManager?.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null
     ) {
         val name = context.getString(R.string.app_name)
         val channel = NotificationChannel(
@@ -43,7 +44,7 @@ fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
             name,
             NotificationManager.IMPORTANCE_DEFAULT
         )
-        notificationManager.createNotificationChannel(channel)
+        notificationManager?.createNotificationChannel(channel)
     }
 
     val intent = ReminderDescriptionActivity.newIntent(context.applicationContext, reminderDataItem)
@@ -64,7 +65,7 @@ fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
         .setAutoCancel(true)
         .build()
 
-    notificationManager.notify(getUniqueId(), notification)
+    notificationManager?.notify(getUniqueId(), notification)
 }
 
 private fun getUniqueId() = ((System.currentTimeMillis() % 10000).toInt())
